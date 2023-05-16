bf88c265dea069523e4e59eae708d6f88df77932
Brenner Paiva <114958953+brennerpaiva@users.noreply.github.com>
2023-05-16 17:11:01 -0300

    configs web and mobile

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-950">
+      <h1 className="p-6 text-4xl font-bold text-zinc-50">
+        Sua cápsula do tempo
+      </h1>
+    </div>
+  )
+}
